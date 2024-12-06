036554d39f5173f2b25aa1c198dc268593aed6fd
Testing <Testing@BT_2S14_20.bktech.local>
2024-12-06 11:05:18 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Time time1 = new Time(8, 9, 58);
+        System.out.println(time1);
+
+    }
+}
---
+++ src/Time.java
@@ -0,0 +1,38 @@
+public class Time {
+    private int seconds;
+    private int hours;
+    private int minutes;
+
+    public Time (int hours, int minutes, int seconds){
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
+
+    public void tick (){
+        seconds++;
+        if (seconds == 60){
+            seconds = 0;
+            minutes ++;
+        }
+        if (minutes == 60){
+            minutes = 0;
+            hours++;
+        }
+        if (hours == 24){
+            seconds = 0;
+            minutes = 0;
+            hours = 0;
+        }
+    }
+
+    @Override
+    public String toString() {
+        if (hours<10){
+            hours = 
+        }
+        if (hours<10||minutes<10||seconds<10) {
+            return "0" + hours + ":0" + minutes + ":0" + seconds;
+        }
+    }
+}
