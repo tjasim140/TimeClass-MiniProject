3d501bddc52146b1c4e3a9b313a4d0cc589c34d2
Tasnia <@gmail.com>
2024-12-27 18:58:43 -0500

    Added test cases to runner class Main, added Javadocs to Time class, finished toString method to properly pad values less than 10 to return string value formatted in 00:00:00 form, added the add method to add hours, seconds, and minutes to an instance of Time given another Time object

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,71 @@
 public class Main {
     public static void main(String[] args) {
         Time time1 = new Time(8, 9, 58);
-        System.out.println(time1);
+        System.out.println("time1: "+time1);
+
+        //Tests changing seconds to minutes
+        time1.tick();
+        time1.tick();
+        System.out.println("\nExpected: 08:10:00");
+        System.out.println("Actual: "+time1);
+
+        //Tests adding seconds
+        time1.tick();
+        System.out.println("\nExpected: 08:10:01");
+        System.out.println("Actual: "+time1);
+
+        time1.tick();
+        System.out.println("\nExpected: 08:10:02");
+        System.out.println("Actual: "+time1);
+
+        time1.tick();
+        System.out.println("\nExpected: 08:10:03");
+        System.out.println("Actual: "+time1);
+
+        Time time2 = new Time(8,59,58);
+        System.out.println("\ntime2: "+time2);
+
+        //Tests minutes to hours
+        time2.tick();
+        time2.tick();
+        System.out.println("\nExpected: 09:00:00");
+        System.out.println("Actual: "+time2);
+
+        Time time3 = new Time (23,59,59);
+        System.out.println("\ntime3: "+time3);
+
+        //Tests resetting clock to exact midnight
+        time3.tick();
+        System.out.println("\nExpected: 00:00:00");
+        System.out.println("Actual: "+time3);
+
+        //Tests that clock starts back at seconds once reset after tick method is called
+        time3.tick();
+        System.out.println("\nExpected: 00:00:01");
+        System.out.println("Actual: "+time3);
+
+        Time time4 = new Time(10, 14, 43);
+        System.out.println("\ntime4: "+time4);
+
+        Time time5 = new Time(8, 30, 29);
+        System.out.println("time5: "+time5);
+
+        //Tests add method
+        time4.add(time5);
+        System.out.println("\nExpected: 18:45:12");
+        System.out.println("Actual: "+time4);
+        System.out.println("\nExpected: 08:30:29");
+        System.out.println("Actual:"+time5);
+
+        Time time6 = new Time(7, 20, 0);
+        System.out.println("\ntime6: "+time6);
+
+        //Tests adding time up to a time past exact midnight
+        time4.add(time6);
+        System.out.println("\nExpected: 02:05:12");
+        System.out.println("Actual: "+time4);
+
+
 
     }
 }
--- src/Time.java
+++ src/Time.java
@@ -1,14 +1,28 @@
+/**
+ * the Time class represents a given time based on a 24-hour clock with seconds, minutes, and hours
+ */
 public class Time {
     private int seconds;
     private int hours;
     private int minutes;
 
+    /**
+     * Constructor for the Time class creating a new instance based on the below parameters
+     * @param hours represents the number of hours on the time
+     * @param minutes represents the number of minutes on the time
+     * @param seconds represents the number of seconds on the time
+     */
     public Time (int hours, int minutes, int seconds){
         this.hours = hours;
         this.minutes = minutes;
         this.seconds = seconds;
     }
 
+    /**
+     * The tick method of the Time class updates the time of an instance by adding a second to the instance variable seconds.
+     * If seconds is greater than or equal to 60 the tick method resets seconds to 0 and adds 1 to minutes, and does the same to hours when minutes becomes greater than or equal to 60.
+     * If hours equals to 24 then the time resets all instance variables to 0 to represent exact midnight.
+     */
     public void tick (){
         seconds++;
         if (seconds == 60){
@@ -24,16 +38,70 @@ public class Time {
             minutes = 0;
             hours = 0;
         }
-        //create strings for each to pad value if <10 then use that for toString
     }
 
+    /**
+     * the add method of the Time class takes the hours, minutes, and seconds instance variables of one
+     * given instance of the Time class and adds it to the instance variables of another instance of the
+     * Time class respectively.
+     * @param time represents another instance of the Time class to be added to another instance
+     *             and is not updated by the method
+     */
+    public void add (Time time){
+        //updates this instances values, not updating time object's values
+        this.hours+= time.hours;
+        this.minutes+= time.minutes;
+        this.seconds += time.seconds;
+
+        //Makes sure no values are greater than or equal to 60 and hours are never equal to 24
+        if (seconds >= 60){
+            minutes += seconds/60;
+            seconds = seconds%60;
+        }
+        if (minutes >= 60){
+            hours+=minutes/60;
+            minutes = minutes%60;
+        }
+        if (hours >= 24){
+            hours = hours-24;
+        }
+
+    }
+
+    /**
+     * toString method for the Time class. This method will return a String padding the hours, minutes, and seconds
+     * of the instance so that the time is returned in 00:00:00 format
+     * @return returns a String in a proper format containing all
+     *         the information of hours, minutes, and seconds of a Time
+     */
     @Override
     public String toString() {
+        //String variables to store instance variables as String values
+        String sHours;
+        String sMinutes;
+        String sSeconds;
+
+        //if hours,minutes,or seconds are not a double-digit value, then a 0 is concatenated in front of the int value
         if (hours<10){
-            hours =
+            sHours = "0"+hours;
         }
-        if (hours<10||minutes<10||seconds<10) {
-            return "0" + hours + ":0" + minutes + ":0" + seconds;
+        else {
+            sHours = "" + hours;
         }
+        if (minutes<10){
+            sMinutes = "0"+minutes;
+        }
+        else{
+            sMinutes = ""+minutes;
+        }
+        if (seconds<10){
+            sSeconds = "0"+seconds;
+        }
+        else {
+            sSeconds = ""+seconds;
+        }
+
+        //returns padded values in 24-Hour clock format
+        return sHours + ":" + sMinutes+ ":" +sSeconds;
     }
 }
